e1f59c8b6ee7b2478cc4a8dae37b4a19c6b29a47
Josenaldo <josenaldo.asj@gmail.com>
2019-12-01 20:26:51 -0300

    Implement a less dumb main method

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,10 @@
 #include "Championship.h"
 #include <iostream>
-#include <map>
+#include <vector>
 #include <iomanip>
 #include <string>
+#include <algorithm>
+
 #define ID_AJAX 1
 #define ID_CHELSEA 2
 #define ID_LILLE 3
@@ -12,6 +14,16 @@
 #define NAME_CHELSEA "Chelsea"
 #define NAME_LILLE "Lille"
 #define NAME_VALENCIA "Valencia"
+
+#define ADDGAME(t1,t2,g1,g2) Championship__addNewGame(t1.id, t2.id, g1,g2)
+#define POS(t) Championship__teamPosition(t.id, &t.pos)
+#define PTS(t) Championship__getPoints(t.id, &t.pt)
+#define WINS(t) Championship__getWins(t.id, &t.w)
+#define DRAWS(t) Championship__getDraws(t.id, &t.d)
+#define LOSSES(t) Championship__getLosses(t.id, &t.l)
+#define GF(t) Championship__getGoalsFor(t.id, &t.gf)
+#define GA(t) Championship__getGoalsAgainst(t.id, &t.ga)
+#define GD(t) Championship__getGoalDiff(t.id, &t.gd)
 
 using namespace std;
 
@@ -26,101 +38,69 @@ struct Team{
     int gf;
     int ga;
     int gd;
+
+    Team(int id, string name){
+        this->id = id;
+        this->name = name;
+        pos = pt = w = d = l = gf = ga = gd = 0;
+    }
 };
 
-// < position , Team >
-map<int, int> positions;
+
+bool comp(Team& t1, Team& t2){
+    if(t1.pt < t2.pt) return true;
+    if(t1.pt > t2.pt) return false;
+    if(t1.w < t2.w) return true;
+    if(t1.w > t2.w) return false;
+    if(t1.gd < t2.gd) return true;
+    if(t1.gd > t2.gd) return false;
+    if(t1.gf < t2.gf) return true;
+    if(t1.gf > t2.gf) return false;
+    return false;
+}
 
 int main(){
-    int posAjax, posChelsea, posLille, posValencia;
-    int ptAjax, ptChelsea, ptLille, ptValencia;
-    int wAjax, wChelsea, wLille, wValencia;
-    int dAjax, dChelsea, dLille, dValencia;
-    int lAjax, lChelsea, lLille, lValencia;
-    int gfAjax, gfChelsea, gfLille, gfValencia;
-    int gaAjax, gaChelsea, gaLille, gaValencia;
-    int gdAjax, gdChelsea, gdLille, gdValencia;
+    // Creates teams
+    Team ajax = Team(0, "Ajax");
+    Team valencia = Team(1, "Valencia");
+    Team chelsea = Team(2, "Chelsea");
+    Team lille = Team(3, "Lille");
+    vector<Team> teams = {ajax,valencia,chelsea,lille};
 
     // Adds games
-    Championship__addNewGame(ID_CHELSEA,ID_VALENCIA,0,1);
-    Championship__addNewGame(ID_AJAX, ID_LILLE, 3,0);
+    ADDGAME(chelsea, valencia, 0, 1);
+    ADDGAME(ajax, lille, 3, 0);
 
-    Championship__addNewGame(ID_LILLE,ID_CHELSEA,1,2);
-    Championship__addNewGame(ID_VALENCIA, ID_AJAX, 0,3);
+    ADDGAME(valencia, ajax, 0,3);
+    ADDGAME(lille,chelsea,1,2);
 
-    Championship__addNewGame(ID_AJAX,ID_CHELSEA,0,1);
-    Championship__addNewGame(ID_LILLE, ID_VALENCIA, 1,1);
+    ADDGAME(ajax,chelsea,0,1);
+    ADDGAME(lille,valencia,1,1);
 
-    Championship__addNewGame(ID_CHELSEA,ID_AJAX,4,4);
-    Championship__addNewGame(ID_VALENCIA, ID_LILLE, 4,1);
+    ADDGAME(chelsea,ajax,4,4);
+    ADDGAME(valencia, lille, 4,1);
 
-    //Championship__editGame(ID_CHELSEA, ID_AJAX, 1,4);
+    ADDGAME(valencia,chelsea,2,2);
+    ADDGAME(lille,ajax,0,2);
 
-    // Computes positions
-    Championship__teamPosition(ID_AJAX,&posAjax);
-    Championship__teamPosition(ID_CHELSEA,&posChelsea);
-    Championship__teamPosition(ID_LILLE,&posLille);
-    Championship__teamPosition(ID_VALENCIA,&posValencia);
+    for(auto& team : teams){
+        POS(team);
+        PTS(team);
+        WINS(team);
+        DRAWS(team);
+        LOSSES(team);
+        GF(team);
+        GA(team);
+        GD(team);
+    }
 
+    sort(teams.begin(), teams.end(), comp);
 
-    // Orders positions
-    positions[posAjax] = ID_AJAX;
-    positions[posChelsea] = ID_CHELSEA;
-    positions[posLille] = ID_LILLE;
-    positions[posValencia] = ID_VALENCIA;
-
-    // Gets points
-    Championship__getPoints(ID_AJAX, &ptAjax);
-    Championship__getPoints(ID_CHELSEA, &ptChelsea);
-    Championship__getPoints(ID_LILLE, &ptLille);
-    Championship__getPoints(ID_VALENCIA, &ptValencia);
-
-    // Get wins
-    Championship__getWins(ID_AJAX, &wAjax);
-    Championship__getWins(ID_CHELSEA, &wChelsea);
-    Championship__getWins(ID_LILLE, &wLille);
-    Championship__getWins(ID_VALENCIA, &wValencia);
-
-    // Get draws
-    Championship__getDraws(ID_AJAX, &dAjax);
-    Championship__getDraws(ID_CHELSEA, &dChelsea);
-    Championship__getDraws(ID_LILLE, &dLille);
-    Championship__getDraws(ID_VALENCIA, &dValencia);
-
-    // Get losses
-    Championship__getLosses(ID_AJAX, &lAjax);
-    Championship__getLosses(ID_CHELSEA, &lChelsea);
-    Championship__getLosses(ID_LILLE, &lLille);
-    Championship__getLosses(ID_VALENCIA, &lValencia);
-
-    // Get goals for
-    Championship__getGoalsFor(ID_AJAX, &gfAjax);
-    Championship__getGoalsFor(ID_CHELSEA, &gfChelsea);
-    Championship__getGoalsFor(ID_LILLE, &gfLille);
-    Championship__getGoalsFor(ID_VALENCIA, &gfValencia);
-
-    // Get goals against
-    Championship__getGoalsAgainst(ID_AJAX, &gaAjax);
-    Championship__getGoalsAgainst(ID_CHELSEA, &gaChelsea);
-    Championship__getGoalsAgainst(ID_LILLE, &gaLille);
-    Championship__getGoalsAgainst(ID_VALENCIA, &gaValencia);
-
-    // Get goal difference
-    Championship__getGoalDiff(ID_AJAX, &gdAjax);
-    Championship__getGoalDiff(ID_CHELSEA, &gdChelsea);
-    Championship__getGoalDiff(ID_LILLE, &gdLille);
-    Championship__getGoalDiff(ID_VALENCIA, &gdValencia);
-
-    for(auto it : positions){
-        cout << it.first << " ";
-        if(it.second == ID_AJAX)
-            cout << left << setw(8) << NAME_AJAX << right << "   " << setw(3) << ptAjax << " | " << setw(3) << wAjax << " | " << setw(3) << dAjax << " | " << setw(3) << lAjax << " | " << setw(3) << gfAjax << " | " << setw(3) << gaAjax << " | " << setw(4) << gdAjax <<  endl;
-        else if(it.second == ID_CHELSEA)
-            cout << left << setw(8) << NAME_CHELSEA << right << "   " << setw(3) << ptChelsea << " | " << setw(3) << wChelsea << " | " << setw(3) << dChelsea << " | " << setw(3) << lChelsea << " | " << setw(3) << gfChelsea << " | " << setw(3) << gaChelsea << " | " << setw(4) << gdChelsea <<  endl;
-        else if(it.second == ID_LILLE)
-            cout << left << setw(8) << NAME_LILLE << right << "   " << setw(3) << ptLille << " | " << setw(3) << wLille << " | " << setw(3) << dLille << " | " << setw(3) << lLille << " | " << setw(3) << gfLille << " | " << setw(3) << gaLille << " | " << setw(4) << gdLille <<  endl;
-        else if(it.second == ID_VALENCIA)
-            cout << left << setw(8) << NAME_VALENCIA << right << "   " << setw(3) << ptValencia << " | " << setw(3) << wValencia << " | " << setw(3) << dValencia << " | " << setw(3) << lValencia << " | " << setw(3) << gfValencia << " | " << setw(3) << gaValencia << " | " << setw(4) << gdValencia <<  endl;
+    int pos = 0;
+    for(auto it = teams.end()-1; it >= teams.begin(); --it){
+        const auto team = *it;
+        cout << ++pos << " ";
+        cout << left << setw(8) << team.name << right << "   " << setw(3) << team.pt << " | " << setw(3) << team.w << " | " << setw(3) << team.d << " | " << setw(3) << team.l << " | " << setw(3) << team.gf << " | " << setw(3) << team.ga << " | " << setw(4) << team.gd <<  endl;
     }
 
     return 0;
